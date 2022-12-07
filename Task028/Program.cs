af4d81c9bc2ff4930e5bb7d1bc8d5df4ab6ecbaa
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-07 18:35:39 +0800

    add Task028

---
+++ Task028/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 28: Напишите программу,
+//которая принимает на вход число N и выдаёт
+//произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120\
+
+Console.Clear();
+Console.Write("Введите  число:");
+int limit = int.Parse (Console.ReadLine());
+Console.WriteLine($"произведение чисел от 1 до {limit} равно {GetSum(limit)}");
+ int GetSum(int limit)
+ {
+    int sum = 1;
+    for (int i = 1; i <= limit; i++)
+    
+        sum = sum * i;
+    return sum;
+ }
