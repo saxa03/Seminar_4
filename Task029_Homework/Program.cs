6d3162bb4e5e2e712bb359022142e00887af585b
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-08 11:38:48 +0800

    add Task 029 HomeWork

---
+++ Task029_Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и
+// выводит их на экран.
+
+//1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33] 
+
+Console.Clear();
+int[] array = GetRandomArray(8);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int[] GetRandomArray(int size)
+{
+    int[] result = new int  [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(1,100);
+    }
+
+    return result;
+    
+}
+
